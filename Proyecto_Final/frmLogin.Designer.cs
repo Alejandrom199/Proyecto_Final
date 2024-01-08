c9b86ce70df4a4fa4726ce570824572570dc4cac
yunitop26 <domenica.bravoa@ug.edu.ec>
2024-01-08 15:26:15 -0500

    hay que agregar mas cosas como las imagenes y eso pilas

--- Proyecto_Final/frmLogin.Designer.cs
+++ Proyecto_Final/frmLogin.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmLogin";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            button1 = new Button();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            groupBox1 = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 7);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(679, 413);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Bisque;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(166, 17);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(330, 394);
+            panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Perpetua Titling MT", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(91, 327);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 33);
+            button1.TabIndex = 5;
+            button1.Text = "Ingresar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Perpetua Titling MT", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(91, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 48);
+            label1.TabIndex = 4;
+            label1.Text = "LOGIN";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(106, 242);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(180, 27);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "contrasena";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(106, 176);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(180, 27);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "usuario";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(49, 232);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(51, 46);
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(49, 166);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(51, 46);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(panel1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Location = new Point(108, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(680, 426);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(989, 535);
+            Controls.Add(groupBox1);
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private GroupBox groupBox1;
     }
 }
