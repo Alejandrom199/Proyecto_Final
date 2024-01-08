678e1113b5109398bb3c10074dea73bce8e1cdee
Saul Maldonado <alejandrom199916@gmail.com>
2024-01-08 13:58:55 -0500

    Cree  clase ejemplo y cambios en el form1 ........

--- Proyecto_Final/Form1.Designer.cs
+++ Proyecto_Final/Form1.Designer.cs
@@ -30,13 +30,17 @@
         {
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(633, 333);
+            button1.Location = new Point(791, 416);
+            button1.Margin = new Padding(4, 4, 4, 4);
             button1.Name = "button1";
-            button1.Size = new Size(140, 91);
+            button1.Size = new Size(175, 114);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
@@ -44,19 +48,51 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(41, 32);
+            label1.Location = new Point(51, 40);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(38, 20);
+            label1.Size = new Size(46, 25);
             label1.TabIndex = 1;
             label1.Text = "hola";
             // 
+            // button2
+            // 
+            button2.Location = new Point(133, 263);
+            button2.Name = "button2";
+            button2.Size = new Size(530, 197);
+            button2.TabIndex = 2;
+            button2.Text = "Cilantro";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(466, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(309, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -67,5 +103,8 @@
 
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
     }
 }
