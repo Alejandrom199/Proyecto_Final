1c260bf84d4c2451527107d21249b8f9288632f9
Saúl Maldonado lópez <106854444+Alejandrom199@users.noreply.github.com>
2024-01-09 18:55:40 -0500

    Empezando frmMecanico

---
+++ Proyecto_Final/frmMecanico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVisual
+{
+    public partial class frmMecanico : Form
+    {
+        public frmMecanico()
+        {
+            InitializeComponent();
+        }
+    }
+}
