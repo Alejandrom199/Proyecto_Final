117ca592d7537c8f80918f5a7eff3a12d95e1cfd
Saúl Maldonado lópez <106854444+Alejandrom199@users.noreply.github.com>
2024-01-09 19:46:30 -0500

    Crud mecanico completo

--- Proyecto_Final/frmMecanico.Designer.cs
+++ Proyecto_Final/frmMecanico.Designer.cs
@@ -28,18 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox2 = new GroupBox();
+            btnCancelar = new Button();
+            btnEliminar = new Button();
+            btnModificar = new Button();
+            btnAgregar = new Button();
+            btnNuevo = new Button();
+            groupBox1 = new GroupBox();
+            tbxCedula = new TextBox();
+            tbxEspecialidad = new TextBox();
+            tbxExperiencia = new TextBox();
+            tbxApellido = new TextBox();
+            tbxNombre = new TextBox();
+            tbxId = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            dgvMecanicos = new DataGridView();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvMecanicos).BeginInit();
             SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnCancelar);
+            groupBox2.Controls.Add(btnEliminar);
+            groupBox2.Controls.Add(btnModificar);
+            groupBox2.Controls.Add(btnAgregar);
+            groupBox2.Controls.Add(btnNuevo);
+            groupBox2.Location = new Point(833, 11);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(188, 215);
+            groupBox2.TabIndex = 10;
+            groupBox2.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(42, 175);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(112, 34);
+            btnCancelar.TabIndex = 6;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(42, 135);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(112, 34);
+            btnEliminar.TabIndex = 5;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Location = new Point(42, 95);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(112, 34);
+            btnModificar.TabIndex = 4;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            btnModificar.Click += btnModificar_Click;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(42, 55);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(112, 34);
+            btnAgregar.TabIndex = 3;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.Location = new Point(42, 15);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(112, 34);
+            btnNuevo.TabIndex = 2;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tbxCedula);
+            groupBox1.Controls.Add(tbxEspecialidad);
+            groupBox1.Controls.Add(tbxExperiencia);
+            groupBox1.Controls.Add(tbxApellido);
+            groupBox1.Controls.Add(tbxNombre);
+            groupBox1.Controls.Add(tbxId);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 11);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(807, 215);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Mecanico";
+            // 
+            // tbxCedula
+            // 
+            tbxCedula.Location = new Point(635, 98);
+            tbxCedula.Name = "tbxCedula";
+            tbxCedula.Size = new Size(150, 31);
+            tbxCedula.TabIndex = 11;
+            // 
+            // tbxEspecialidad
+            // 
+            tbxEspecialidad.Location = new Point(128, 160);
+            tbxEspecialidad.Name = "tbxEspecialidad";
+            tbxEspecialidad.Size = new Size(150, 31);
+            tbxEspecialidad.TabIndex = 10;
+            // 
+            // tbxExperiencia
+            // 
+            tbxExperiencia.Location = new Point(393, 160);
+            tbxExperiencia.Name = "tbxExperiencia";
+            tbxExperiencia.Size = new Size(150, 31);
+            tbxExperiencia.TabIndex = 9;
+            // 
+            // tbxApellido
+            // 
+            tbxApellido.Location = new Point(393, 101);
+            tbxApellido.Name = "tbxApellido";
+            tbxApellido.Size = new Size(150, 31);
+            tbxApellido.TabIndex = 8;
+            // 
+            // tbxNombre
+            // 
+            tbxNombre.Location = new Point(128, 101);
+            tbxNombre.Name = "tbxNombre";
+            tbxNombre.Size = new Size(150, 31);
+            tbxNombre.TabIndex = 7;
+            // 
+            // tbxId
+            // 
+            tbxId.Location = new Point(128, 48);
+            tbxId.Name = "tbxId";
+            tbxId.Size = new Size(102, 31);
+            tbxId.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 163);
+            label6.Name = "label6";
+            label6.Size = new Size(113, 25);
+            label6.TabIndex = 5;
+            label6.Text = "Especialidad:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(284, 160);
+            label5.Name = "label5";
+            label5.Size = new Size(103, 25);
+            label5.TabIndex = 4;
+            label5.Text = "Experiencia:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(559, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Cedula:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(305, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Apellido:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(88, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 25);
+            label1.TabIndex = 0;
+            label1.Text = "ID:";
+            // 
+            // dgvMecanicos
+            // 
+            dgvMecanicos.AllowUserToAddRows = false;
+            dgvMecanicos.AllowUserToDeleteRows = false;
+            dgvMecanicos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMecanicos.Location = new Point(12, 242);
+            dgvMecanicos.Name = "dgvMecanicos";
+            dgvMecanicos.ReadOnly = true;
+            dgvMecanicos.RowHeadersWidth = 62;
+            dgvMecanicos.RowTemplate.Height = 33;
+            dgvMecanicos.Size = new Size(1010, 277);
+            dgvMecanicos.TabIndex = 8;
+            dgvMecanicos.CellClick += dgvMecanicos_CellClick;
             // 
             // frmMecanico
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1034, 531);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(dgvMecanicos);
             Name = "frmMecanico";
             Text = "frmMecanico";
+            Load += frmMecanico_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvMecanicos).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox2;
+        private Button btnCancelar;
+        private Button btnEliminar;
+        private Button btnModificar;
+        private Button btnAgregar;
+        private Button btnNuevo;
+        private GroupBox groupBox1;
+        private TextBox tbxCedula;
+        private TextBox tbxEspecialidad;
+        private TextBox tbxExperiencia;
+        private TextBox tbxApellido;
+        private TextBox tbxNombre;
+        private TextBox tbxId;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DataGridView dgvMecanicos;
     }
 }
--- Proyecto_Final/frmMecanico.cs
+++ Proyecto_Final/frmMecanico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,202 @@ namespace CapaVisual
 {
     public partial class frmMecanico : Form
     {
+        CN_Mecanico obj_mecanico = new CN_Mecanico();
         public frmMecanico()
         {
             InitializeComponent();
+            btnCancelar.Enabled = true;
+            MaximizeBox = false;
+            tbxId.ReadOnly = true;
+        }
+
+        private void frmMecanico_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvMecanicos.AllowUserToAddRows = false;
+                CargarGridMecanicos();
+
+                DisableCampos(true);
+
+                btnNuevo.Enabled = true;
+                btnAgregar.Enabled = false;
+                btnModificar.Enabled = false;
+                btnEliminar.Enabled = false;
+                btnCancelar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DisableCampos(bool valor)
+        {
+            if (valor)
+            {
+                tbxId.Enabled = false;
+                tbxNombre.Enabled = false;
+                tbxApellido.Enabled = false;
+                tbxCedula.Enabled = false;
+                tbxEspecialidad.Enabled = false;
+                tbxExperiencia.Enabled = false;
+            }
+            else
+            {
+                tbxId.Enabled = true;
+                tbxNombre.Enabled = true;
+                tbxApellido.Enabled = true;
+                tbxCedula.Enabled = true;
+                tbxEspecialidad.Enabled = true;
+                tbxExperiencia.Enabled = true;
+            }
+        }
+
+        private bool isNuevo;
+
+        private void SetearCampos()
+        {
+            tbxId.Text = string.Empty;
+            tbxNombre.Text = string.Empty;
+            tbxApellido.Text = string.Empty;
+            tbxCedula.Text = string.Empty;
+            tbxEspecialidad.Text = string.Empty;
+            tbxExperiencia.Text = string.Empty;
+        }
+        private void CargarGridMecanicos()
+        {
+            try
+            {
+                dgvMecanicos.DataSource = obj_mecanico.GetListaMecanicos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            isNuevo = true;
+            SetearCampos();
+
+            btnNuevo.Enabled = false;
+            btnAgregar.Enabled = true;
+            btnModificar.Enabled = false;
+            btnEliminar.Enabled = false;
+
+            DisableCampos(false);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (isNuevo)
+            {
+                try
+                {
+                    obj_mecanico.Nombre = tbxNombre.Text;
+                    obj_mecanico.Apellido = tbxApellido.Text;
+                    obj_mecanico.Cedula = tbxCedula.Text;
+                    obj_mecanico.Especialidad = tbxEspecialidad.Text;
+                    obj_mecanico.Experiencia = tbxExperiencia.Text;
+
+                    var resultado = obj_mecanico.CrearMecanico(obj_mecanico);
+
+                    if (resultado)
+                    {
+                        MessageBox.Show("Registro Creado con Éxito.");
+                        CargarGridMecanicos();
+                        DisableCampos(true);
+
+                        btnNuevo.Enabled = true;
+                        btnAgregar.Enabled = false;
+                        btnModificar.Enabled = false;
+                        btnEliminar.Enabled = false;
+                    }
+                    else { }
+
+                    isNuevo = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Rellene todos los datos");
+                }
+
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (!isNuevo)
+            {
+                obj_mecanico.Id = Convert.ToInt32(tbxId.Text);
+                obj_mecanico.Nombre = tbxNombre.Text;
+                obj_mecanico.Apellido = tbxApellido.Text;
+                obj_mecanico.Cedula = tbxCedula.Text;
+                obj_mecanico.Especialidad = tbxEspecialidad.Text;
+                obj_mecanico.Experiencia = tbxExperiencia.Text;
+
+                if (obj_mecanico.ModificarMecanico(obj_mecanico))
+                {
+                    MessageBox.Show("Registro actualizado!");
+                    CargarGridMecanicos();
+                    DisableCampos(true);
+
+                    btnNuevo.Enabled = true;
+                    btnAgregar.Enabled = false;
+                    btnModificar.Enabled = false;
+                    btnEliminar.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Registro no pudo ser actualizado!");
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            obj_mecanico.Id = Convert.ToInt32(tbxId.Text);
+
+            if (obj_mecanico.EliminarMecanico(obj_mecanico))
+            {
+                MessageBox.Show("Registro eliminado!");
+                CargarGridMecanicos();
+
+                btnNuevo.Enabled = true;
+                btnAgregar.Enabled = false;
+                btnModificar.Enabled = false;
+                btnEliminar.Enabled = false;
+
+                DisableCampos(true);
+            }
+            else
+            {
+                MessageBox.Show("Registro no pudo ser eliminado!");
+            }
+        }
+
+        private void dgvMecanicos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DisableCampos(false);
+            isNuevo = false;
+
+            if (e.RowIndex != -1)
+            {
+                tbxId.Text = dgvMecanicos.Rows[e.RowIndex].Cells[0].Value.ToString();
+                tbxNombre.Text = dgvMecanicos.Rows[e.RowIndex].Cells[1].Value.ToString();
+                tbxApellido.Text = dgvMecanicos.Rows[e.RowIndex].Cells[2].Value.ToString();
+                tbxCedula.Text = dgvMecanicos.Rows[e.RowIndex].Cells[3].Value.ToString();
+                tbxEspecialidad.Text = dgvMecanicos.Rows[e.RowIndex].Cells[4].Value.ToString();
+                tbxExperiencia.Text = dgvMecanicos.Rows[e.RowIndex].Cells[5].Value.ToString();
+
+                btnNuevo.Enabled = false;
+                btnAgregar.Enabled = false;
+                btnModificar.Enabled = true;
+                btnEliminar.Enabled = true;
+            }
+            else { }
         }
     }
 }
